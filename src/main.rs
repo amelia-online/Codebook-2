8887e3c6ad952664cc678b5aaa4f9b151127dae0
Amelia Johnson <u1052331@umail.utah.edu>
2023-04-25 22:52:23 -0600

    Implemented simple file interpreter mode.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
 use std::io;
+use std::io::Read;
 use std::io::stdout;
 use std::io::Write;
 
 pub mod interpreter;
+use interpreter::interpet::interpret_line;
 use interpreter::session::SessionInfo;
 use interpreter::interpet;
 
+fn arguments() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut index = 1;
+    while index < args.len() {
+        let arg = &args[index];
+        match arg.as_str() {
+
+            "f" => {
+                index += 1;
+                if let Ok(mut file) = std::fs::File::open(&args[index]) {
+                    let mut buffer = String::new();
+                    file.read_to_string(&mut buffer).expect("");
+                    let mut session = SessionInfo::new();
+                    interpret_line(buffer, &mut session);
+                    std::process::exit(0);
+                } else {
+                    panic!("File not found!");
+                }
+            }
+
+
+            _ => panic!("Unrecognized argument '{arg}'"),
+        }
+    }
+}
+
 fn main() {
+    arguments();
     use interpet::*;
     println!();
     println!("Welcome to Codebook 2.0! The programmable CLI calculator.");
